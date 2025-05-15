7ea03dd582eefc978bfcaae77769feb8f3019dfc
Pietro Tiago <166723178+PedPietro@users.noreply.github.com>
2025-05-15 09:03:29 -0300

    Arquivos Principais
    Aqui estão os programas principais que vamos usar para ter um melhor controle de Viagem e Passgeiros

---
+++ mainPassageiro.py
@@ -0,0 +1,21 @@
+class ManutençãoPassageiros:
+
+    def __init__():
+        
+
+
+    def incluir():
+        pass
+
+    def excluir():
+        pass
+
+    def alterar():
+        pass
+
+    def buscar():
+        pass
+
+    def listar():
+        pass
+
