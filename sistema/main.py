39c48e9478540ed4a875c28411fda0766061de34
Betinha <162441834+ElizabethTrindade@users.noreply.github.com>
2025-05-22 09:17:14 -0300

    main e inicio de viagem

--- sistema/main.py
+++ sistema/main.py
@@ -1,5 +1,6 @@
 #add funçao conecta bd aq no principal bbzudos
-
+from viagem import viagem
+import os
 def seletorDeOpcoes():
     print("Em qual sistema vc deseja mexer?")
     print("1 - Cadastro de Passageiros")
@@ -8,3 +9,48 @@ def seletorDeOpcoes():
     print("4 - Registro de inicio e de final de viagem, liberando passageiros e vagas")
     #opçoes para o balcão presencial(nn web)
     #preencher tabelas diretamente no SSMS usando insrt e nn por programação
+    
+    opcao = str(input("Escolha: "))
+    match opcao:
+        case "1": op1()
+        case "2": op2()
+        case "3": op3()
+        case "4": op4()
+        
+    espera = input("Pressione Enter para finalizar...")
+    print("Obrigado pelo uso!")
+    
+    
+def op1():
+    pass
+    
+def op2():
+    pass
+    
+def op3():
+    pass
+    
+    
+def op4():
+    #Registro de inicio e de final de viagem, liberando passageiros e vagas
+    os.system('cls') or None
+    print("Selecione uma opção:")
+    print("0 - Finalizar")
+    print("1 - Registro do Inicio da Viagem")
+    print("2 - Registro do Final da Viagem")
+    print("3 - Liberar Passageiros e Vagas")
+            
+    escolha = str (input("Escolha: "))
+    passageiros = viagem()
+    match escolha:
+        case "1": passageiros.Registroinicio()
+        case "2": passageiros.Registrofinal()
+        case "3": passageiros.Liberarpassageirosevagas()
+                
+    espera = input("Pressione Enter para finalizar...")
+    print("Obrigado pelo uso!")
+        
+        
+        
+if __name__ == '__main__':
+    seletorDeOpcoes()
--- sistema/viagem.py
+++ sistema/viagem.py
@@ -1,33 +1,14 @@
 import os
 class viagem():
-    def __init__():
-        pass
+    def __init__(self):
+        self.status = "planejada"
     
-    def seletordeopcoes():
-        #Registro de inicio e de final de viagem, liberando passageiros e vagas
-        os.system('cls') or None
-        print("Selecione uma opção:")
-        print("0 - Finalizar")
-        print("1 - Registro do Inicio da Viagem")
-        print("2 - Registro do Final da Viagem")
-        print("3 - Liberar Passageiros e Vagas")
-        
-        escolha = str (input("Escolha: "))
-        match escolha:
-            case "1": Registroinicio()
-            case "2": Registrofinal()
-            case "3": Liberarpassageirosevagas()
-            
-            
-        
-        
-    def Registroinicio():
-        pass
+    def Registroinicio(self):
+        self.status = "Em andamento"
 
-    def Registrofinal():
-        pass
+    def Registrofinal(self):
+        self.status = "Finalizada"
     
-    def Liberarpassageirosevagas():
+    def Liberarpassageirosevagas(self):
         pass
-        
         
