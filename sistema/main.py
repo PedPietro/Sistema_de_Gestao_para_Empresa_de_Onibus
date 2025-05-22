bd5f1ca9c1a70c716319e6cbbabaf4a2837bd6bb
Pietro Tiago <166723178+PedPietro@users.noreply.github.com>
2025-05-22 08:37:27 -0300

    Criação arquivos para entidades do sistema

--- sistema/main.py
+++ sistema/main.py
@@ -1,1 +1,51 @@
-#add funçao conecta bd aq no principal bbzudos
+import os
+import pyodbc as bd
+import getpass as gp
+import motorista
+import onibus
+import passageiros
+import passagens
+import viagem
+import UF
+import cidade
+
+def conectouAoBancoDeDados() -> bool: # informará se conseguiu ou não conectar
+    global conexao
+    os.system('cls') or None
+    senha = gp.getpass("Digite a senha do seu banco de dados:") # pede a senha
+    try:
+        conexao = bd.connect(driver="{SQL Server}",
+                        server="regulus.cotuca.unicamp.br",
+                        database="BDseuRA",
+                        uid="BDseuRA", # seu username no servidor de BD
+                        pwd=f"{senha}") # substitui variável senha
+                        # pela senha digitada
+        print("Conexão bem sucedida!")
+        return True
+    except:
+        print("Não foi possível conectar ao banco de dados")
+        return False
+
+def seletor(): # CRUD: Create Retrieve Update Delete
+    opcao = 1
+    while opcao != 0:
+        os.system('cls') or None
+        print("1 - Motorista")
+        print("2 - Ônibus")
+        print("3 - Passageiro")
+        print("4 - Viagem")
+        print("5 - Cidade")
+        print("6 - Passagem")
+        print("0 - Sair")
+        opcao = int(input("\nDigite o número da opção desejada:"))
+        match opcao:
+            case "1": motorista()
+            case "2": onibus()
+            case "3": passageiros()
+            case "4": viagem()
+            case "5": cidade()
+            case "6": passagens()
+            case "7": UF()
+
+if __name__ == "__main__":
+    seletor()
---
+++ sistema/onibus.py
@@ -0,0 +1,15 @@
+class ManutencaoOnibus:
+    def incluir():
+        pass
+
+    def excluir():
+        pass
+
+    def alterar():
+        pass
+    
+    def buscar():
+        pass
+
+    def listar():
+        pass
