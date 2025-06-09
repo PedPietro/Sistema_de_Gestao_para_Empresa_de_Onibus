f1d608d27a4f6f9c369af9a63f71ed338dfeeeb1
Eduarda <eduardayacllara@gmail.com>
2025-06-09 11:03:30 -0300

    rafael: none

--- sistema/passageiros.py
+++ sistema/passageiros.py
@@ -212,7 +212,7 @@ class ManutencaoPassageiros:
             print("ID. \tCpf              \tNome                 \tTelefone       \tData De Nascimento     \tEmail")
             for passageiro in registros:
                 #arrumar indexação dessa merda aaaaaaaaaaaaa
-                print(f"{passageiro[0]}   \t{passageiro[1]:20}        \t{passageiro[2]}     \t{passageiro[3]}   \t{passageiro[4]}           \t{passageiro[5]}")
+                print(f"{passageiro[0]}   \t{passageiro[1]}        \t{passageiro[2]}     \t{passageiro[3]}   \t{passageiro[4]}           \t{passageiro[5]}")
             input("Tecle [enter] para terminar:")
 
         meuCursor.commit()
